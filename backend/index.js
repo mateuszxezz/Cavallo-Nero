eaf319ba1d468a19865841154c3902e889072684
Maba <mateusbezerra092008@gmail.com>
2025-05-23 11:58:57 -0300

    first commit

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://cavallo-nero.vercel.app',
+  ],
   credentials: true,
 }));
 
