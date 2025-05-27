4fc9b9e7bf9bf42c743e72a1a70c701023f9dac9
Maba <mateusbezerra092008@gmail.com>
2025-05-27 14:37:32 -0300

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,7 @@
-// index.js
-
 require('dotenv').config(); // Muito importante carregar antes de tudo
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const printfulRoute = require('./routes/printfulRoute');
-app.use('/printfulRoute', printfulRoute);
-
 
 app.use(cors({
   origin: [
@@ -17,6 +12,10 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+// Usa o caminho correto pro printfulRoute:
+const printfulRoutes = require('./src/services/routes/printfulRoute');
+app.use('/printfulRoute', printfulRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
